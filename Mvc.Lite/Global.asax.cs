eaaec4364988381f92218481ca953779f3a25ca8
Wagner Andrade <wagner.andradesilva@gmail.com>
2012-05-17 12:24:02 -0300

    First commit.

---
+++ Mvc.Lite/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace Mvc.Lite
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
